574e6ad1d22468adfc48cb7bbaa5885491ea6d0a
Oxana <vitsvoks@list.ru>
2025-07-26 11:29:02 +0300

    раскладываем по папочкам

---
+++ lesson_01/lesson2_if.py/2.py
@@ -0,0 +1,6 @@
+def dev_by_three(number):
+    return "Да" if number % 3 == 0 else "Нет"
+
+num = int(input("Введите число: "))
+result = dev_by_three(num)
+print(f"Делится ли на три {num}? - {result}")
---
+++ lesson_01/lesson2_if.py/age = 17.py
@@ -0,0 +1,8 @@
+age = 17
+
+print(age < 18)
+
+if (age<18):
+    print("ok")
+
+
