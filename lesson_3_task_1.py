7c1b3ac077c8a61ea2b83b380c8242f3ce5863e1
Oksanastudent24 <vitsvoks@list.ru>
2025-07-26 11:45:30 +0300

    Delete lesson_3_task_1.py

--- lesson_3_task_1.py
+++
@@ -1,9 +0,0 @@
-from user import User
-
-# Создаем экземпляр класса Product
-my_user = User("Василий", "Теркин")
-
-# Вызываем методы и выводим результаты
-print(my_user.get_first_name())
-print(my_user.get_last_name())
-print(my_user.get_user_info())
