ad69b6f7ef9302a10fca56334836f553ddfbe93f
Oxana <vitsvoks@list.ru>
2025-08-11 23:41:12 +0300

    06K_lesson

---
+++ 06K_lesson/test_01_form.py
@@ -0,0 +1,50 @@
+import pytest
+from selenium import webdriver
+#from selenium.webdriver.edge.service import Service as EdgeService
+#from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    yield driver
+    time.sleep(5)
+    #driver.quit()
+
+def test_fill_form(driver):
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+    #time.sleep(5)
+    wait = WebDriverWait(driver, 5)
+
+    # Заполнение формы
+    driver.find_element(By.NAME, "first-name").send_keys("Иван")
+    driver.find_element(By.NAME, "last-name").send_keys("Петров")
+    driver.find_element(By.NAME, "address").send_keys("Ленина, 55-3")
+    driver.find_element(By.NAME, "zip_code").send_keys("") #Оставляем пустым
+    driver.find_element(By.NAME, "city").send_keys("Москва")
+    driver.find_element(By.NAME, "country").send_keys("Россия")
+    driver.find_element(By.NAME, "e-mail").send_keys("test@skypro.com")
+    driver.find_element(By.NAME, "phone_number").send_keys("+7985899998787")
+    driver.find_element(By.NAME, "job-position").send_keys("QA")
+    driver.find_element(By.NAME, "company").send_keys("SkyPro")
+
+    # Нажатие кнопки
+    driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+    wait = WebDriverWait(driver, 10)
+
+    # Проверка подсветки поля Zip code
+    pole_z = driver.find_element(By.ID, "zip-code").get_attribute("class")
+    assert pole_z == "alert py-2 alert-danger"
+
+    # Проверка подсветки остальных полей
+    poles = ["#first-name", "#last-name", "#address", "#city", "#country", "#e-mail", "#phone", "#company"]
+    for pole in poles:
+        pole_class = driver.find_element(By.CSS_SELECTOR, pole).get_attribute("class")
+        assert pole_class == "alert py-2 alert-success"
+
+        driver.quit()
+
